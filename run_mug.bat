dda7852a9f100c49765b958fc8ee02de71634975
julian <julian5595@outlook.com>
2026-07-31 12:12:45 +0100

    Remove generated outputs and add new run scripts/assets

---
+++ run_mug.bat
@@ -0,0 +1,6 @@
+rem usage: ascii_art.py [-h] [-o OUTPUT] [-w WIDTH] [--rotate {0,90,180,-90}] [--show] input_image
+rem python ascii_art.py .\images\rabbit.jpg -o output.txt -w 60 --rotate 90 --show
+
+python ascii_art.py .\images\mug.jpg -o .\outputs\output_mug.txt -w 60 --rotate -90 --show
+
+pause
---
+++ run_rabbit.bat
@@ -0,0 +1,6 @@
+rem usage: ascii_art.py [-h] [-o OUTPUT] [-w WIDTH] [--rotate {0,90,180,-90}] [--show] input_image
+rem python ascii_art.py .\images\rabbit.jpg -o output.txt -w 60 --rotate 90 --show
+
+python ascii_art.py .\images\rabbit.jpg -o .\outputs\output_rabbit.txt -w 60 --rotate -90 --show
+
+pause
